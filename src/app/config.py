c092d22ea3bb164e8d2ce7f3d41ba5295032bed7
Etienne Posthumus <eposthumus@gmail.com>
2022-08-24 12:23:25 +0200

    Update config

--- src/app/config.py
+++ src/app/config.py
@@ -10,6 +10,6 @@ ADMIN_DATABASE = os.environ.get("ADMIN_DATABASE", "admin.sqlite")
 SECRET_KEY = os.environ.get("SECRET_KEY", "foobarbaz")
 ACCESS_TOKEN_EXPIRE_DAYS = int(os.environ.get("ACCESS_TOKEN_EXPIRE_DAYS", "30"))
 
-SITE_URI = "https://pda.epoz.org/"
-CONTACT_EMAIL = "secretariat@cerl.org"
+SITE_URI = "https://pda.cerl.org/"
+CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL", "ep@epoz.org")
 HELP_PATH = os.environ.get("HELP_PATH", "help")

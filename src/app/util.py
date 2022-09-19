ff993987c555a038feb3566b346fb8b53187b8d2
Etienne Posthumus <eposthumus@gmail.com>
2022-09-20 00:41:35 +0200

    Load from database and filters

--- src/app/util.py
+++ src/app/util.py
@@ -1,5 +1,11 @@
-import smtplib
+import smtplib, json
 from email.message import EmailMessage
+import databases
+from .config import DATABASE_URL
+from fastapi import HTTPException
+from jinja2 import Markup
+
+database = databases.Database(DATABASE_URL)
 
 
 def send_email(sender: str, receiver: str, subject: str, body: str):
@@ -25,6 +31,9 @@ field_titles = {
     "PAGE": "Location",
     "TYPE_INS": "Type",
     "TECHNIQUE": "Technique",
+    "WIDTH": "Width",
+    "HEIGHT": "Height",
+    "TITLE": "Title",
 }
 
 
@@ -39,3 +48,45 @@ def TF(obj):
         return t, v
 
     return _TF
+
+
+async def get(objid):
+    row = await database.fetch_one(
+        "SELECT obj FROM source WHERE id = :id", values={"id": objid}
+    )
+
+    if not row:
+        raise HTTPException(status_code=404)
+
+    obj = json.loads(row[0])
+    return obj
+
+
+def load(obj_string):
+    obj = json.loads(obj_string)
+    return obj
+    # Some fields, have IDs in them, filter them.
+    # for k, v in obj.copy().items():
+    #     if k in ("LOCATION_ORIG_CERLID", "OWNERS_CERLID", "INSTIT_CERLID"):
+    #         obj[k] = [x.split("|")[-1].strip(" .,()") for x in obj[k]]
+    # return obj
+
+
+# A jinja filter to strip CERLIDs from a field
+def strip_cerlid(value):
+    tmp = value.split("|")
+    if len(tmp) < 2:
+        return value
+    cerlid = tmp[0]
+    rest = "|".join(tmp[1:])
+    return rest
+
+
+# A Jinja filter to turn a | separated value into a CERL Thesarus link
+def cerl_thesaurus(value):
+    tmp = value.split("|")
+    if len(tmp) < 2:
+        return value
+    cerlid = tmp[0]
+    rest = "|".join(tmp[1:])
+    return Markup(f"<a href='https://data.cerl.org/thesaurus/{cerlid}'>{rest}</a>")

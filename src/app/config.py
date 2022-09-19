589048f604a6245322938af206516ba9accbdec8
Etienne Posthumus <eposthumus@gmail.com>
2022-09-20 00:42:10 +0200

    UPLOADS path

--- src/app/config.py
+++ src/app/config.py
@@ -13,3 +13,4 @@ ACCESS_TOKEN_EXPIRE_DAYS = int(os.environ.get("ACCESS_TOKEN_EXPIRE_DAYS", "30"))
 SITE_URI = "https://pda.cerl.org/"
 CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL", "ep@epoz.org")
 HELP_PATH = os.environ.get("HELP_PATH", "help")
+UPLOADS_PATH = os.environ.get("UPLOADS_PATH")
